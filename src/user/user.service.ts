40beaa89561d67d37c2fe06424305eeaf872f136
nathalia <116982223+Jeffersonm06@users.noreply.github.com>
2024-06-08 20:44:23 -0300

    feat: create module auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -58,4 +58,17 @@ export class UserService {
         }
         return user;
     }
+
+    async findUserByEmail(email: string): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                email: email
+            },
+        });
+
+        if (!user) {
+            throw new NotFoundException(`Email ${email} not found`)
+        }
+        return user;
+    }
 }
